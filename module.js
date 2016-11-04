a067bf090e4fab44b91b887abc1ad4e317839c55
Maksym Rad'ko <maksym.radko@customertimes.com>
2016-11-05 00:07:52 +0200

    add require function

---
+++ module.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function loadModule(filename, module, require) {
+	const wrappedSrc = `(function (module, exports, require) {
+		${fs.readFileSync(filename, 'utf-8')}
+	})(module, module.exports, require);`;
+	eval(wrappedSrc);
+}
+
+const require = (moduleName) => {
+	console.log(`require invoked for module ${moduleName}`);
+	const id = require.resolve(moduleName);
+	
+	if (require.cache[id]) {
+		return require.cache[id].exports;
+	}
+	
+	// module metadata
+	const module = {
+		exports: {},
+		id: id
+	};
+	
+	// update cache
+	require.cache[id] = module;
+	
+	// load module
+	loadModule(id, module, require);
+	
+	// return exported variable
+	return module.exports;
+};
+
+require.cache = {};
+require.resolve = (moduleName) => {};
